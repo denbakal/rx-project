b9f0237c79180399630d7dba49c62631239ca2e8
denbakal <denbakal7@gmail.com>
2019-03-03 00:47:54 +0200

    Concurrency: CompletableFuture: using anyOf()

---
+++ src/main/java/ua/demo/reactive/rx/concurrency/completable/future/callback/any/of/Launcher.java
@@ -0,0 +1,42 @@
+package ua.demo.reactive.rx.concurrency.completable.future.callback.any.of;
+
+import lombok.SneakyThrows;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
+public class Launcher {
+    @SneakyThrows
+    public static void main(String[] args) {
+        CompletableFuture<String> future1 = CompletableFuture.supplyAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                throw new IllegalStateException(e);
+            }
+            return "Result of Future 1";
+        });
+
+        CompletableFuture<String> future2 = CompletableFuture.supplyAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                throw new IllegalStateException(e);
+            }
+            return "Result of Future 2";
+        });
+
+        CompletableFuture<String> future3 = CompletableFuture.supplyAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                throw new IllegalStateException(e);
+            }
+            return "Result of Future 3";
+        });
+
+        CompletableFuture<Object> anyOfFuture = CompletableFuture.anyOf(future1, future2, future3);
+
+        System.out.println(anyOfFuture.get()); // Result of Future 2
+    }
+}
